cf25f11eea40d7fbbe77f892447d67afb7c6ac68
Victor Bezak <victor.bezak@laborchart.com>
2022-10-29 23:53:31 -0500

    update to karma.conf for ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const os = require('os');
+const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -38,15 +41,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: [
-      'ChromeHeadlessNoSandbox'
+      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
     ],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
+      ChromeHeadless: {
+        base: 'Chrome',
         flags: [
           '--no-sandbox',
-          // required to run without privileges in Docker 
-          '--disable-web-security',
+          '--headless',
           '--disable-gpu',
           '--remote-debugging-port=9222'
         ]
